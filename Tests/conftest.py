31802f9df4f15e7cbdae9cd200e82bf6bf094d9f
satishtrantor <satish.kumar@trantorinc.com>
2022-06-13 17:49:14 +0530

    changed conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,16 +1,31 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.utils import ChromeType
 from Config.config import TestData
 
 
 @pytest.fixture(params=["chrome"], scope='function')
 def init_driver(request):
     if request.param == "chrome":
-        #web_driver = webdriver.Chrome(service=Service(executable_path=TestData.CHROME_EXECUTABLE_PATH))
-        web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        web_driver = webdriver.Chrome(service=Service(executable_path=TestData.CHROME_EXECUTABLE_PATH))
+        # chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+        # chrome_options = Options()
+        # options = [
+        #     "--headless",
+        #     "--disable-gpu",
+        #     "--window-size=1920,1200",
+        #     "--ignore-certificate-errors",
+        #     "--disable-extensions",
+        #     "--no-sandbox",
+        #     "--disable-dev-shm-usage"
+        # ]
+        # for option in options:
+        #     chrome_options.add_argument(option)
+        # web_driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
         web_driver.implicitly_wait(10)
         web_driver.delete_all_cookies()
         web_driver.maximize_window()
@@ -18,4 +33,3 @@ def init_driver(request):
         request.cls.driver = web_driver
         yield
         web_driver.quit()
-
